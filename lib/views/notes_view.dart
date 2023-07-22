0615e93abf47ebfd2737c5cb7a0374a62209d5d8
samer <sameradel789@gmail.com>
2023-07-22 12:58:56 +0300

    build notes_view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,1 +1,12 @@
-class NotesView {}
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
