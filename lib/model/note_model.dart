699955306bcd3233eb0840b4ffb18f9e71cca97d
samer <sameradel789@gmail.com>
2023-07-27 17:33:01 +0300

    create generated hive file

--- lib/model/note_model.dart
+++ lib/model/note_model.dart
@@ -1,11 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
-class NoteModel {
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
   NoteModel(
     this.title,
