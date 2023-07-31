623e1152e04ee92bc5851d4b0aa4e1bc25908281
samer <sameradel789@gmail.com>
2023-07-31 14:19:26 +0300

    final Versoin of NotesApp

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/consts.dart';
+import 'package:notes_app/cubit/add_cubit/note_cubit.dart';
 import 'package:notes_app/cubit/add_cubit/simple_bloc_observer.dart';
 import 'package:notes_app/cubit/fetch_notes_cubit/fetch_notes_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
@@ -21,8 +22,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => FetchNotesCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => NoteCubit()),
+        BlocProvider(create: (context) => FetchNotesCubit())
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(brightness: Brightness.dark, fontFamily: "Poppins"),
--- lib/views/edit_view.dart
+++ lib/views/edit_view.dart
@@ -2,8 +2,10 @@ import 'package:awesome_icons/awesome_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubit/add_cubit/note_cubit.dart';
 import 'package:notes_app/cubit/fetch_notes_cubit/fetch_notes_cubit.dart';
 import 'package:notes_app/model/note_model.dart';
+import 'package:notes_app/widgets/add_note_form.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -35,8 +37,13 @@ class _EditViewState extends State<EditView> {
                 onsubmit: () {
                   widget.note.title = newtitle ?? widget.note.title;
                   widget.note.content = newcontent ?? widget.note.content;
+                  widget.note.color =
+                      BlocProvider.of<NoteCubit>(context).notecolor.value;
                   Navigator.pop(context);
-                  // widget.note.save();
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: const Text("Note Editted succesfullt"),
+                    backgroundColor: Color(widget.note.color),
+                  ));
                   BlocProvider.of<FetchNotesCubit>(context).fetchAllNotes();
                 },
                 title: "Edit Note",
@@ -63,6 +70,7 @@ class _EditViewState extends State<EditView> {
                 hint: "Content",
                 maxlines: 12,
               ),
+              ColorListView()
             ],
           ),
         ),
